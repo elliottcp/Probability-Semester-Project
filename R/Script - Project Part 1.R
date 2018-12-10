dac14f43ee2e0aa251e4868349af84effc79c960
unknown <charles.elliott@slu.edu>
2018-12-10 14:24:19 -0600

    Trying to incorporate the ability for the user to enter a function like dgamma() or dbeta(), rather than the expression for f(x).

--- R/Script - Project Part 1.R
+++ R/Script - Project Part 1.R
@@ -1,3 +1,6 @@
+install.packages("devtools")
+library(devtools)
+
 THEFUNCTION1D <- function(pdf, n, lower_bound, upper_bound, C) {
 
   if(n == "Dr. Speegle is the best!") stop("You bet he is!")
@@ -8,8 +11,16 @@ THEFUNCTION1D <- function(pdf, n, lower_bound, upper_bound, C) {
   if(lower_bound > upper_bound) stop("That's an error.  The lower_bound is greater than the upper_bound.")
 
   n <- ceiling(n)
-  pdf_function <- function(x) {
-    eval(parse(text = pdf))}
+  if(is.character(pdf) == TRUE) {
+    pdf_function <- function(x) {
+      eval(parse(text = pdf))
+    }
+  }
+  if(is.character(pdf) != TRUE) {
+    pdf_function <- function(x, ...) {
+      pdf(x, ...)
+    }
+  }
 
   sample_function <- function(pdf_function, lower_bound, upper_bound, C) {
     sample <- c()
@@ -27,17 +38,41 @@ THEFUNCTION1D <- function(pdf, n, lower_bound, upper_bound, C) {
 }
 
 
-normal_sample <- THEFUNCTION1D(pdf = "1/((2*pi)^0.5)*exp((-x^2)/2)", n = 10000, lower_bound = -5, upper_bound = 5, C = 0.4)
-mean(normal_sample)
-
-THEFUNCTION1D(pdf = "1/((2*pi)^0.5)*exp((-x^2)/2)", n = 20, lower_bound = -10, upper_bound = 10, C = 0.4)
 
 
+#Standard Normal distribution:
+THEFUNCTION1D(pdf = "1/((2*pi)^0.5)*exp((-x^2)/2)", n = 10, lower_bound = -5, upper_bound = 5, C = 0.4)
+
+#Uniform distribution(a=-10, b=10):
+THEFUNCTION1D(pdf = "0.05", n = 20, lower_bound = -10, upper_bound = 10, C = 0.06)
+
+#Exponential(lambda = 7):
+THEFUNCTION1D(pdf = "(1/7)*exp((-1/7)*x)", n = 8, lower_bound = 0, upper_bound = 100, C = 50)
+
+#Gamma()
+THEFUNCTION1D(pdf = dgamma(x,1,1), n = 8, lower_bound = 0, upper_bound = 1, C = 10)
+
+
+pdf_function <- function(x,...) {
+  dbeta(x, ...)
+}
+
+x <- c(1)
+THEFUNCTION1D(pdf = dbeta(x,1,1), n = 8, lower_bound = 0, upper_bound = 1, C = 10)
+
+
+test <- function(x) {x^2}
+
+?dbeta
+
+pdf_function(1)
 
 THEFUNCTION1D(pdf = "1/((2*pi)^0.5)*exp((-x^2)/2)", n = 1, lower_bound = -4, upper_bound = 4, C = 0.4)
 THEFUNCTION1D(pdf = "x^2", n = 100, lower_bound = -100, upper_bound = 100, C = 1)
 THEFUNCTION1D(pdf = "0.1", n = 10, lower_bound = 0, upper_bound = 10, C = 0.2)
 
+is.character("hey")
+is.character(45)
 
 #To-do:
 #error test for a custom pdf that's constant.  Run a different set of code then?
